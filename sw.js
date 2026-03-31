a704ec00a4fe3dac3d3f1aa76cd2af5c0e62e2b3
mrbiggs360-maker <mr.biggs360@gmail.com>
2026-03-31 11:19:26 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-const CACHE_NAME='cwai-wear-v1.5',ASSETS=['./','./index.html','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>{if(x!==CACHE_NAME)return caches.delete(x)})))));
+const CACHE_NAME = 'cwai-wear-v1.5',
+ASSETS = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', e => 
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)))
+);
+
+self.addEventListener('fetch', e => 
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
+
+self.addEventListener('activate', e => 
+    e.waitUntil(caches.keys().then(k => 
+        Promise.all(k.map(x => {
+            if(x !== CACHE_NAME) return caches.delete(x)
+        }))
+    ))
+);
